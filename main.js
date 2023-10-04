e6b4b34c3663e9a494ec724693ee56d08eb320d2
Oscar Herrera <a.oscarherrera@gmail.com>
2023-10-04 12:04:59 -0500

    added google maps api

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ const navMenu = document.querySelector('.nav-menu');
 hamburgerMenu.addEventListener('click', () => {
     navMenu.classList.toggle('hide');
 })
+
+
+
+function initMap(){
+
+    // MAP OPTIONS
+    const options = {
+        center: {lat:41.8781 , lng:-87.6298},
+        zoom: 10
+    }
+    
+    // NEW MAP
+    map = new google.maps.Map(document.getElementById("map"),options)
+
+
+    //MARKER
+
+    const marker = new google.maps.Marker({
+        position:{lat:41.8674 , lng:-87.6275},
+        map:map
+
+    })
+
+}
